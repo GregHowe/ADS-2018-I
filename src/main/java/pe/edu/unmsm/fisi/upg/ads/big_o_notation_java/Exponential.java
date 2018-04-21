97abc8051550ea321d1070a68205dd4231f4ef61
Omar <richardfc83@gmail.com>
2018-04-21 14:13:28 -0500

    Test Send

---
+++ src/main/java/pe/edu/unmsm/fisi/upg/ads/big_o_notation_java/Exponential.java
@@ -0,0 +1,71 @@
+package pe.edu.unmsm.fisi.upg.ads.big_o_notation_java;
+
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Stopwatch;
+
+//Complexity: O(2^N)
+public class Exponential {
+	private final static int N = 90;
+	private long[] fibonacciCache = new long[N + 1];
+	
+	public static void main(String[] args) {
+		long N = 80;
+		Exponential exponential = new Exponential();
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		for (int i = 1; i <= N; i++) {
+			try {
+				long fibonacci = exponential.fibonacciCache(i);
+				System.out.println(i + " => " + fibonacci);
+			} catch (Exception e) {
+				e.printStackTrace();
+			} 
+        }
+		System.out.println(String.format("Time elapsed: %d seconds", stopwatch.elapsed(TimeUnit.SECONDS)) + "\n");
+	}
+	
+	public long fibonacci(int n) throws Exception {
+        if (n < 0) {
+            throw new Exception("N can not be less than zero");
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+	
+	public long fibonacciLineal(int n) throws Exception {
+        if (n < 0) {
+            throw new Exception("N can not be less than zero");
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        long fibonacci = 0;
+        long previous = 1;
+        long penultimate = 0;
+        
+        for ( int i = 1; i<=n ; i++ )
+        {
+        	penultimate = fibonacci;
+        	fibonacci = previous + fibonacci;
+        	previous = penultimate;
+        }
+        return fibonacci;
+    }
+	
+	  public long fibonacciCache(int N) throws Exception {
+	        if (N < 0) {
+	            throw new Exception("N can not be less than zero");
+	        }
+	        if (N <= 2) {
+	            fibonacciCache[N] = 1;
+	        }
+	        if (fibonacciCache[N] == 0) {
+	            fibonacciCache[N] = fibonacciCache(N - 1) + fibonacciCache(N - 2);
+	        }
+	        return fibonacciCache[N];
+	    }
+	
+	
+}
